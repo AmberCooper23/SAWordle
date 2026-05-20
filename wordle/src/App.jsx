747c03a1c42ca5e48d35e1091e96acf3b4ce5297
AmberCooper23 <2677396@students.wits.ac.za>
2026-05-20 19:03:27 +0200

    setup

---
+++ wordle/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <main className='app'>
+      <header className='appHeader'>
+        <h1 className='appTitle'>SA WORDLE</h1>
+        <p className='appHint'>Today's wordle has 5 letters!</p>
+      </header>
+    </main>
+  )
+}
+
+export default App
